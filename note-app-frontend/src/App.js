743ab77e83ae66842db3987b37553b35aebaaaf6
stalin670 <amitalvin01@gmail.com>
2024-08-09 16:25:51 +0530

    Remove Unnecessary Code

--- note-app-frontend/src/App.js
+++ note-app-frontend/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import AllRoutes from './routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes />
+      Hello
     </div>
   );
 }
--- note-app-frontend/src/routes/AllRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Route , Routes } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-
-export default function AllRoutes() {
-    return <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-    </Routes>
-}
